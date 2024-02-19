d5568404a12bd47fa97084e5f5bc90ffa87efd62
moataz-armash <mo3taz314@gmail.com>
2024-02-19 23:50:30 +0300

    Add navbar

---
+++ src/components/Navbar/ButtonConfigrations.js
@@ -0,0 +1,20 @@
+export const buttonStyles = {
+  color: "black",
+  opacity: "0.5",
+  width: "80px",
+  "&:hover": {
+    backgroundColor: "transparent",
+    opacity: "1",
+    color: "black",
+    borderRadius: "0",
+    padding: "0",
+  },
+};
+
+export const buttonConfig = [
+  { label: "Home", width: "80px", target: "Home" },
+  { label: "Our Services", width: "140px", target: "Services" },
+  { label: "Join Us", width: "100px", target: "Join" },
+  { label: "FAQs", width: "100px", target: "Faqs" },
+  { label: "Contact", width: "100px", target: "Contact" },
+];
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { buttonStyles, buttonConfig } from "./ButtonConfigrations";
+import { AppBar, Toolbar, Stack, Button, Box } from "@mui/material";
+import { CardMedia } from "@mui/material";
+import logo from "../../assets/logo.png";
+
+const Navbar = () => {
+  const [selectedHeader, setSelectedHeader] = useState(null);
+
+  const handleSelect = (selectedButton) => {
+    setSelectedHeader(selectedButton);
+  };
+
+  return (
+    <Stack direction="row" justifyContent="space-between" alignItems="center">
+      <CardMedia
+        component="img"
+        sx={{ width: "200px", marginTop: "10px" }}
+        image={logo}
+        alt="logo"
+      />
+      <AppBar
+        position="static"
+        sx={{
+          width: "68%",
+          borderRadius: "50px",
+          alignItems: "center",
+          backgroundColor: "#EFEFEF",
+        }}>
+        <Toolbar>
+          <Box
+            sx={{
+              display: {
+                xs: "none",
+                md: "block",
+              },
+            }}>
+            <Stack direction="row" spacing={10}>
+              {buttonConfig.map((button) => (
+                <Button
+                  key={button.label}
+                  sx={{
+                    ...buttonStyles,
+                    width: button.width,
+                    ...(selectedHeader === button.target && {
+                      backgroundColor: "black",
+                      opacity: "1",
+                      color: "white",
+                      borderRadius: "40px",
+                      padding: "10px 20px",
+                      "&:hover": {
+                        backgroundColor: "black",
+                        opacity: "1",
+                        color: "white",
+                        borderRadius: "40px",
+                        padding: "10px 20px",
+                      },
+                    }),
+                  }}
+                  onClick={() => handleSelect(button.target)}>
+                  {button.label}
+                </Button>
+              ))}
+            </Stack>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Stack>
+  );
+};
+
+export default Navbar;
